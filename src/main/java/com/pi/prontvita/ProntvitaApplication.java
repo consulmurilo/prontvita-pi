107399f558bfd2326dd333ed687b51739958d827
Murilo Consul <muriloconsul@icloud.com>
2025-05-01 17:51:10 -0300

    Versão inicial do projeto com estrutura definida

---
+++ src/main/java/com/pi/prontvita/ProntvitaApplication.java
@@ -0,0 +1,13 @@
+package com.pi.prontvita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntvitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntvitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/prontvita/ProntvitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.prontvita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProntvitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
